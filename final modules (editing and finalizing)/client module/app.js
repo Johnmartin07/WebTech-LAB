a2146c25d0b1b7ddd3945f67ccd4e92270d65144
patrick <patrick>
2018-05-20 22:49:20 +0800

    adding changes in client module

--- final modules (editing and finalizing)/client module/app.js
+++ final modules (editing and finalizing)/client module/app.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const mysql = require('mysql');
-const http = require('http');
 const path = require('path');
 
 const app = express();
 
 app.use(express.static(path.join('public')));
 
-app.set('view engine', 'pug');
+var bodyParser = require('body-parser')
+app.use(bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
+app.set('view engine', 'ejs');
 app.set('views', './view');
 
 //create connection to MySQL
@@ -23,76 +28,87 @@ db.connect((err) => {
 	if(err){
 		throw err
 	}
-	console.log('Mysql Connected..');
+	console.log('Successfully Connected to MusifydB..');
 });
 
 //Connect to Musify website
-app.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname + '/index.html'));
+app.get('/index', function (req, res) {
+	db.query('SELECT * FROM musify.package where pack_status = "available"',function(err,results,fields){
+		// console.log(results);
+		res.render('index',{package: results});
+	})	
+});
+
+//Connect to Musify website
+app.get('/index/:uid', function (req, res) {
+	uid = req.params.uid;
+
+	res.redirect('/')
+});
+
+app.get('/transaction', function (req, res) {
+	db.query(`SELECT payment.*,customer.firstname,customer.lastname,customer.address,package.pack_name FROM musify.payment
+	inner join musify.package on package.pack_id = payment.pack_id
+	inner join musify.customer on customer.cust_id = payment.cust_id
+	WHERE payment.pack_id = package.pack_id and payment.pay_status = "paid"`,function (err, results, fields) {
+	// console.log(results);
+	res.render('transaction',{transaction: results});
+	})
 });
 
 app.get('/editProfile', function (req, res) {
-	 let view = request.query.view;
+	var uid = 2;
 
-	let db = mysql.createConnection({
-		host: 'localhost',
-		user: 'root',
-		password: '',
-		database: 'musify'
-	});
-
-	const query = `SELECT firstname,lastname,username,email,address
-			   	   FROM customer
-			   	   WHERE username = 'ikaw'`;
-
-	db.connectionn(err =>{
-		if(err) throw err;
-
-		db.query(query,[username],(err, result, fields) =>{
-			if (err) throw err;
-
-			switch(view) {
-				case 'html':
-				response.render('editProfile');
-
-				default:
-					response.redirect(error404.html);
-					break;
-			}
-		});
-	}); 	   
-
+	db.query('SELECT firstname,lastname,username,email,address FROM musify.customer WHERE cust_id = ?',[uid],function(err,results,fields){
+		// console.log(results);
+			res.render('editProfile',{customer: results[0] });
+	})
 });
 
-//Insert package
-app.get('/addpost1', (req,res) =>{
-	let port = {title: 'Package One', body:'First Package'};
-	let sql = 'INSERT INTO package SET ?';
-	let query = db.query(sql, post, (err, result) =>{
-		if (err) throw err;
-		console.log(result);
-		res.send('Package one added');
-	});
+app.post('/editProfile/save:uid', function (req, res) {
+	var uid = req.params.uid;
+	 fname = req.body.fname,
+	 lname = req.body.lname,
+	 uname = req.body.uname,
+	 password = req.body.password,
+	 password = req.body.password,
+	 email = req.body.email,
+	 address = req.body.address;
+	console.log(req.body);
+	res.redirect('editProfile', + fname + lname + uname + password + password + email + address);
 });
 
-//select package
-app.get('/getposts', (req, res) => {
-    let sql = 'SELECT * FROM package';
-    let query = db.query(sql, (err, results) => {
-        if(err) throw err;
-        console.log(results);
-        res.send('Posts fetched...');
-    });
-});
+app.get('/customerRequests', function (req, res) {
 
-// Select single post
-app.get('/getpost/:id', (req, res) => {
-    let sql = `SELECT * FROM package  = ${req.params.id}`;
-    let query = db.query(sql, (err, result) => {
-        if(err) throw err;
-        console.log(result);
-        res.send('Package fetched...');
-    });
+		db.query(`SELECT package.pack_name,rentals.*,customer.firstname,customer.lastname,customer.address FROM musify.rentals
+	inner join musify.customer on customer.cust_id = rentals.cust_id
+    inner join musify.package on rentals.pack_id = package.pack_id
+	WHERE rentals.cust_id = customer.cust_id and rentals.rent_status = "pending"`,function (err, results, fields) {
+	pending = results;
+		db.query(`SELECT package.pack_name,rentals.*,customer.firstname,customer.lastname,customer.address FROM musify.rentals
+	inner join musify.customer on customer.cust_id = rentals.cust_id
+    inner join musify.package on rentals.pack_id = package.pack_id
+	WHERE rentals.cust_id = customer.cust_id and rentals.rent_status = "accepted"`,function (err, results, fields) {
+	accepted = results;
+		db.query(`SELECT package.pack_name,rentals.*,customer.firstname,customer.lastname,customer.address FROM musify.rentals
+	inner join musify.customer on customer.cust_id = rentals.cust_id
+    inner join musify.package on rentals.pack_id = package.pack_id
+	WHERE rentals.cust_id = customer.cust_id and rentals.rent_status = "declined"`,function (err, results, fields) {
+	declined = results;
+		db.query(`SELECT package.pack_name,rentals.*,customer.firstname,customer.lastname,customer.address FROM musify.rentals
+	inner join musify.customer on customer.cust_id = rentals.cust_id
+    inner join musify.package on rentals.pack_id = package.pack_id
+	WHERE rentals.cust_id = customer.cust_id and rentals.rent_status = "done"`,function (err, results, fields) {
+	done = results;
+		console.log(accepted);
+		console.log(pending);
+		console.log(declined);
+		console.log(done);
+		res.render('customerRequests',{pending: pending, accepted: accepted, declined: declined, done: done});
+		})
+		})
+		})
+		})
 });
 
 app.listen('2001', () =>{
